15a3595e09083aa524777870ec9f147a635e97cf
Juan Edi <jedi@manas.com.ar>
2016-12-30 19:10:59 -0300

    Remove password for docker database engine.
    This is to keep our local test environment consistent with the settings
    used in CI.

--- config/dev.exs
+++ config/dev.exs
@@ -37,7 +37,7 @@ config :phoenix, :stacktrace_depth, 20
 config :registro, Registro.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: "postgres",
-  password: "postgres",
+  password: "",
   database: "registro_dev",
   hostname: "localhost",
   pool_size: 10

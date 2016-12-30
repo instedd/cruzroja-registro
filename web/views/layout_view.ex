291058319462a5b3bfd6e951939d887510c29d52
Juan Edi <jedi@manas.com.ar>
2016-12-30 18:58:01 -0300

    Display flash messages with toasts

--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -1,3 +1,12 @@
 defmodule Registro.LayoutView do
   use Registro.Web, :view
+
+  def toast_flash_message(conn, flash_section) do
+    if get_flash(conn, flash_section) do
+      content_tag(:script) do
+        {:safe, encoded_msg} = raw(Poison.encode!(get_flash(conn, flash_section)))
+        raw("Materialize.toast(#{encoded_msg}, 8000)")
+      end
+    end
+  end
 end

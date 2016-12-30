6facb568ef075aa317f00756711c4e3f30fb4d99
Santiago Palladino <spalladino@gmail.com>
2016-12-30 15:12:20 -0300

    Configuration for production environment

--- config/prod.exs
+++ config/prod.exs
@@ -13,49 +13,27 @@ use Mix.Config
 # which you typically run after static files are built.
 config :registro, Registro.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
+  url: [host: System.get_env("HOST") || "cruzroja.instedd.org", port: 80],
+  secret_key_base: System.get_env("SECRET_KEY_BASE"),
   cache_static_manifest: "priv/static/manifest.json"
 
 # Do not print debug messages in production
 config :logger, level: :info
 
-# ## SSL Support
-#
-# To get SSL working, you will need to add the `https` key
-# to the previous section and set your `:url` port to 443:
-#
-#     config :registro, Registro.Endpoint,
-#       ...
-#       url: [host: "example.com", port: 443],
-#       https: [port: 443,
-#               keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
-#               certfile: System.get_env("SOME_APP_SSL_CERT_PATH")]
-#
-# Where those two env variables return an absolute path to
-# the key and cert in disk or a relative path inside priv,
-# for example "priv/ssl/server.key".
-#
-# We also recommend setting `force_ssl`, ensuring no data is
-# ever sent via http, always redirecting to https:
-#
-#     config :registro, Registro.Endpoint,
-#       force_ssl: [hsts: true]
-#
-# Check `Plug.SSL` for all available options in `force_ssl`.
+# Configure database
+config :registro, Registro.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: System.get_env("DATABASE_USER") || "cruzroja",
+  password: System.get_env("DATABASE_PASS"),
+  database: System.get_env("DATABASE_NAME") || "cruzroja",
+  hostname: System.get_env("DATABASE_HOST") || "localhost",
+  pool_size: 10
 
-# ## Using releases
-#
-# If you are doing OTP releases, you need to instruct Phoenix
-# to start the server for all endpoints:
-#
-#     config :phoenix, :serve_endpoints, true
-#
-# Alternatively, you can configure exactly which server to
-# start per endpoint:
-#
-#     config :registro, Registro.Endpoint, server: true
-#
-
-# Finally import the config/prod.secret.exs
-# which should be versioned separately.
-import_config "prod.secret.exs"
+# Configure coherence mailer
+config :coherence, Registro.Coherence.Mailer,
+  adapter: Swoosh.Adapters.SMTP,
+  relay: System.get_env("SMTP_RELAY"),
+  username: System.get_env("SMTP_USER"),
+  password: System.get_env("SMTP_PASS"),
+  tls: :always,
+  auth: :always

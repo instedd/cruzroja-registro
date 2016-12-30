dcfe78bfdae1890526c92970a8bbad66aa8f423f
Santiago Palladino <spalladino@gmail.com>
2016-12-30 15:22:53 -0300

    Add travis configuration

--- config/test.exs
+++ config/test.exs
@@ -13,7 +13,7 @@ config :logger, level: :warn
 config :registro, Registro.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: "postgres",
-  password: "postgres",
+  password: "",
   database: "registro_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox

6dcf734fbfc88b13872e73937f04612eddb68caa
Juan Edi <jedi@manas.com.ar>
2017-01-04 12:08:27 -0300

    Remove old code

--- web/plugs/Authorization.ex
+++ web/plugs/Authorization.ex
@@ -4,15 +4,8 @@ defmodule Registro.Authorization do
 
   alias Registro.User
   alias Registro.Router.Helpers, as: Routes
-  require Logger
 
   def init(default), do: default
-
-  defmacro check_path(conn, pattern, do: exp) do
-    quote do
-      if(Regex.match?(unquote(pattern), unquote(conn).request_path), do: unquote(exp))
-    end
-  end
 
   def call(conn, opts) do
     case opts[:check].(conn.assigns[:current_user].role) do

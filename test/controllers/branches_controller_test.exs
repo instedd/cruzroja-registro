8ea8389a53f1ef956d4cd61ffeec7d07bfd6cc7d
Juan Edi <jedi@manas.com.ar>
2017-01-16 11:02:58 -0300

    #11 Branch admin can list his branches

--- test/controllers/branches_controller_test.exs
+++ test/controllers/branches_controller_test.exs
@@ -4,40 +4,61 @@ defmodule Registro.BranchesControllerTest do
   import Registro.ModelTestHelpers
   import Registro.ControllerTestHelpers
 
-  test "verifies that user is logged in", %{conn: conn} do
-    conn = get conn, "/filiales"
-    assert redirected_to(conn) == "/"
+  alias Registro.Branch
+
+  describe "listing" do
+    test "verifies that user is logged in", %{conn: conn} do
+      conn = get conn, "/filiales"
+      assert redirected_to(conn) == "/"
+    end
+
+    test "does not allow non-admin users", %{conn: conn} do
+      setup_db
+
+      conn = conn
+      |> log_in("john@example.com")
+      |> get("/filiales")
+
+      assert_unauthorized(conn)
+    end
+
+    test "displays all branches to super_admin user", %{conn: conn} do
+      setup_db
+
+      conn = conn
+      |> log_in("admin@instedd.org")
+      |> get("/filiales")
+
+      assert html_response(conn, 200)
+      assert (Enum.count conn.assigns[:branches]) == 2
+    end
+
+    test "branch admin can only see his administrated branches", %{conn: conn} do
+      setup_db
+
+      conn = conn
+      |> log_in("branch1@instedd.org")
+      |> get("/filiales")
+
+      assert html_response(conn, 200)
+
+      branch_names = conn.assigns[:branches]
+      |> Enum.map(&(&1.name))
+
+      assert branch_names == ["Branch 1"]
+    end
   end
 
-  test "does not allow non-admin users", %{conn: conn} do
+  test "a branch admin cannot access a branch he doesn't administrate", %{conn: conn} do
     setup_db
 
+    branch2 = Repo.get_by!(Branch, name: "Branch 2")
+
     conn = conn
-    |> log_in("john@example.com")
-    |> get("/filiales")
+         |> log_in("branch1@instedd.org")
+         |> get(branches_path(conn, :show, branch2))
 
     assert_unauthorized(conn)
-  end
-
-  test "does not allow branch_admin", %{conn: conn} do
-    setup_db
-
-    conn = conn
-    |> log_in("branch@instedd.org")
-    |> get("/filiales")
-
-    assert_unauthorized(conn)
-  end
-
-  test "displays all branches to super_admin user", %{conn: conn} do
-    setup_db
-
-    conn = conn
-    |> log_in("admin@instedd.org")
-    |> get("/filiales")
-
-    assert html_response(conn, 200)
-    assert (Enum.count conn.assigns[:branches]) == 2
   end
 
   def setup_db do
@@ -46,7 +67,7 @@ defmodule Registro.BranchesControllerTest do
 
     create_user(email: "john@example.com", role: "volunteer", branch_id: branch1.id)
     create_super_admin(email: "admin@instedd.org")
-    create_branch_admin(email: "branch@instedd.org", branch: branch1)
+    create_branch_admin(email: "branch1@instedd.org", branch: branch1)
   end
 
 end
--- web/controllers/branches_controller.ex
+++ web/controllers/branches_controller.ex
@@ -2,28 +2,38 @@ defmodule Registro.BranchesController do
   use Registro.Web, :controller
 
   alias __MODULE__
-  alias Registro.Repo
-  alias Registro.Branch
-  alias Registro.Pagination
+  alias Registro.{Repo, Branch, Pagination, Datasheet, User}
 
-  plug Registro.Authorization, check: &BranchesController.authorize_request/2
+  plug Registro.Authorization, [ check: &BranchesController.authorize_listing/2 ] when action in [:index]
+  plug Registro.Authorization, [ check: &BranchesController.authorize_detail/2 ] when action in [:show, :update]
 
   def index(conn, params) do
     import Ecto.Query
 
+    datasheet = Coherence.current_user(conn).datasheet
+
+    query = if datasheet.is_super_admin do
+              from b in Branch
+            else
+              branch_ids = datasheet.admin_branches |> Enum.map(&(&1.id))
+
+              from b in Branch, where: b.id in ^branch_ids
+            end
+
+    query = from b in query, order_by: :name
+
     page = Pagination.requested_page(params)
-    total_count = Repo.aggregate(Branch, :count, :id)
+    total_count = Repo.aggregate(query, :count, :id)
     page_count = Pagination.page_count(total_count)
+    branches = Pagination.all(query, page_number: page)
 
     {template, conn} = case params["raw"] do
-                       nil ->
-                         { "index.html", conn }
+                         nil ->
+                           { "index.html", conn }
 
-                       _   ->
-                         { "listing.html", put_layout(conn, false) }
-                     end
-
-    branches = Pagination.all((from b in Branch, order_by: :name), page_number: page)
+                         _   ->
+                           { "listing.html", put_layout(conn, false) }
+                       end
 
     render(conn, template,
       branches: branches,
@@ -73,7 +83,13 @@ defmodule Registro.BranchesController do
     end
   end
 
-  def authorize_request(_conn, current_user) do
-    current_user.datasheet.is_super_admin
+  def authorize_listing(_conn, %User{datasheet: datasheet}) do
+    Datasheet.is_admin?(datasheet)
+  end
+
+  def authorize_detail(conn, %User{datasheet: datasheet}) do
+    branch_id = String.to_integer(conn.params["id"])
+
+    datasheet.is_super_admin || Datasheet.is_admin_of?(datasheet, branch_id)
   end
 end

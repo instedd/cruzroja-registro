b993d1a4358c6385f436072d50d64a84006326fb
Juan Edi <jedi@manas.com.ar>
2017-01-17 18:01:05 -0300

    #16 Use materialize error styles in registration form

--- web/views/view_helpers.ex
+++ web/views/view_helpers.ex
@@ -13,4 +13,47 @@ defmodule Registro.ViewHelpers do
     end
   end
 
+  def validated_input(f, name, attrs) do
+    attrs = add_class_if_field_error(f, name, attrs, "invalid")
+
+    text_input f, name, attrs
+  end
+
+  def validated_password_input(f, name, attrs) do
+    attrs = add_class_if_field_error(f, name, attrs, "invalid")
+
+    password_input f, name, attrs
+  end
+
+  def validated_label(f, name, opts, attrs \\ []) do
+    text = opts[:text] || humanize(name)
+    attrs = add_msg_if_field_error(f, name, attrs)
+
+    label f, name, attrs do
+      case opts[:required] do
+        true ->
+          ["#{text}\n", content_tag(:abbr, "*", class: "required", title: "required")]
+        _ ->
+          text
+      end
+    end
+  end
+
+  defp add_msg_if_field_error(f, name, attrs) do
+    case f.errors[name] do
+      nil ->
+        attrs
+      error ->
+        [{:'data-error', Registro.ErrorHelpers.translate_error(error)} | attrs]
+    end
+  end
+
+  defp add_class_if_field_error(f, name, attrs, class) do
+    case f.errors[name] do
+      nil ->
+        attrs
+      _error ->
+        Keyword.put(attrs, :class, "#{attrs[:class]} #{class}")
+    end
+  end
 end

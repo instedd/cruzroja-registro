f3da2591e6b105e06408e9a66dc323f74ddafb42
Juan Edi <jedi@manas.com.ar>
2017-02-14 10:01:18 -0300

    #63 Tidy up

--- web/models/branch_management.ex
+++ web/models/branch_management.ex
@@ -1,31 +1,33 @@
 defmodule Registro.BranchManagement do
 
-  alias __MODULE__
-  alias Registro.Datasheet
-  alias Registro.Repo
-  alias Registro.Branch
-  alias Registro.Invitation
-  alias Registro.UserAuditLogEntry
+  alias Registro.{
+    Branch,
+    BranchManagement,
+    Datasheet,
+    Invitation,
+    Repo,
+    UserAuditLogEntry,
+  }
 
   def update_staff(changeset, current_user, admin_emails, clerk_emails) do
-    {preexisting_admin_datasheets, new_admin_datasheets} = existing_and_new_from_emails(admin_emails)
-    {preexisting_clerk_datasheets, new_clerk_datasheets} = existing_and_new_from_emails(clerk_emails)
+    {preexisting_admins, new_admins} = create_missing_datasheets(admin_emails)
+    {preexisting_clerks, new_clerks} = create_missing_datasheets(clerk_emails)
 
-    admin_datasheets = preexisting_admin_datasheets ++ new_admin_datasheets
-    clerk_datasheets = preexisting_clerk_datasheets ++ new_clerk_datasheets
-    new_datasheets = new_admin_datasheets ++ new_clerk_datasheets
+    changeset =
+      changeset
+      |> Branch.update_admins(preexisting_admins ++ new_admins)
+      |> Branch.update_clerks(preexisting_clerks ++ new_clerks)
+      |> validate_admin_not_removing_himself(current_user)
 
-    changeset = changeset
-              |> Branch.update_admins(admin_datasheets)
-              |> Branch.update_clerks(clerk_datasheets)
-              |> validate_admin_not_removing_himself(current_user)
+    new_datasheets = new_admins ++ new_clerks
 
-    %{changeset: changeset, new_datasheets: new_datasheets}
+    %{ changeset: changeset,
+       new_datasheets: new_datasheets }
   end
 
   def log_changes(current_user, changeset) do
-    {added_admins, removed_admins} = Branch.admin_changes(changeset)
-    {added_clerks, removed_clerks} = Branch.clerk_changes(changeset)
+    {added_admins, removed_admins} = admin_changes(changeset)
+    {added_clerks, removed_clerks} = clerk_changes(changeset)
 
     Enum.each(added_admins, fn id ->
       UserAuditLogEntry.add(id, current_user, :branch_admin_granted)
@@ -46,33 +48,37 @@ defmodule Registro.BranchManagement do
     :ok
   end
 
-  def admin_changes(changeset) do
+  defp admin_changes(changeset) do
     previous_admins = changeset.data.admins |> Enum.map(&(&1.id))
     updated_admins  = changeset |> Ecto.Changeset.get_field(:admins) |> Enum.map(&(&1.id))
 
-    added_admins = updated_admins
-    |> Enum.reject(fn id -> Enum.member?(previous_admins, id) end)
+    added_admins =
+      updated_admins
+      |> Enum.reject(fn id -> Enum.member?(previous_admins, id) end)
 
-    removed_admins = previous_admins
-    |> Enum.reject(fn id -> Enum.member?(updated_admins, id) end)
+    removed_admins =
+      previous_admins
+      |> Enum.reject(fn id -> Enum.member?(updated_admins, id) end)
 
     {added_admins, removed_admins}
   end
 
-  def clerk_changes(changeset) do
+  defp clerk_changes(changeset) do
     previous_clerks = changeset.data.clerks |> Enum.map(&(&1.id))
     updated_clerks  = changeset |> Ecto.Changeset.get_field(:clerks) |> Enum.map(&(&1.id))
 
-    added_clerks = updated_clerks
-    |> Enum.reject(fn id -> Enum.member?(previous_clerks, id) end)
+    added_clerks =
+      updated_clerks
+      |> Enum.reject(fn id -> Enum.member?(previous_clerks, id) end)
 
-    removed_clerks = previous_clerks
-    |> Enum.reject(fn id -> Enum.member?(updated_clerks, id) end)
+    removed_clerks =
+      previous_clerks
+      |> Enum.reject(fn id -> Enum.member?(updated_clerks, id) end)
 
     {added_clerks, removed_clerks}
   end
 
-  defp existing_and_new_from_emails(emails) do
+  defp create_missing_datasheets(emails) do
     import Ecto.Query
 
     preexisting_datasheets = Repo.all(from d in Datasheet,
@@ -82,12 +88,12 @@ defmodule Registro.BranchManagement do
       preload: [:user, :invitation]
     )
 
-    preexisting_emails = preexisting_datasheets
-    |> Enum.map(&Datasheet.email/1)
+    preexisting_emails = Enum.map(preexisting_datasheets, &Datasheet.email/1)
 
-    new_datasheets = emails
-    |> Enum.filter(fn(email) -> !Enum.member?(preexisting_emails, email) end)
-    |> Enum.map(&BranchManagement.invite!/1)
+    new_datasheets =
+      emails
+      |> Enum.filter(fn(email) -> !Enum.member?(preexisting_emails, email) end)
+      |> Enum.map(&BranchManagement.invite!/1)
 
     {preexisting_datasheets, new_datasheets}
   end
@@ -108,8 +114,9 @@ defmodule Registro.BranchManagement do
   end
 
   def invite!(email) do
-    invitation = Invitation.new_admin_changeset(email)
-    |> Repo.insert!
+    invitation =
+      Invitation.new_admin_changeset(email)
+      |> Repo.insert!
 
     Registro.ControllerHelpers.send_coherence_email :invitation, invitation, Invitation.accept_url(invitation)
 
